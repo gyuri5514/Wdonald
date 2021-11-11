bace76f145f9280bd86f99fa1afb439370c249e8
holic345 <holic345@naver.com>
2021-11-11 18:09:33 +0900

    dfdf

---
+++ src/main/java/com/wdelivery/menu/winMorningSet/vo/WinMorningSetVO.java
@@ -0,0 +1,23 @@
+package com.wdelivery.menu.winMorningSet.vo;
+
+import java.util.Date;
+
+public class WinMorningSetVO {
+
+	private Integer w_code;
+	private String w_name;
+	private Integer w_price;
+	private int w_kcal;
+	private Date w_regdate;
+	private String w_img_path;
+	private String w_img;
+	private String w_n_img_path;
+	private String w_n_img;
+	private int w_status;
+	private int category_code;
+	private String w_e_name;
+	private String w_detail_comment;
+	private String w_detail_img_path;
+	private String w_allergy;
+	
+}
