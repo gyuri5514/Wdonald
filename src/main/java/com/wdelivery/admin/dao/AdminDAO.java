a875f68dfc45e33270183207816d5d379cede32d
gyuri <gyuri5514@gmail.com>
2021-11-15 19:52:54 +0900

    관리자 스타트!

---
+++ src/main/java/com/wdelivery/admin/dao/AdminDAO.java
@@ -0,0 +1,11 @@
+package com.wdelivery.admin.dao;
+
+import java.util.List;
+
+import com.wdelivery.member.payment.vo.PaymentVO;
+
+public interface AdminDAO {
+
+
+	public List<PaymentVO> indexView();
+}
