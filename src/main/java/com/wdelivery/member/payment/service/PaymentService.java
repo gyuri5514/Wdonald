bac66c47f9298a5271a1770e837210068f9f46a2
kimdon <kimde47@naver.com>
2021-11-21 19:24:07 +0900

    뭐했지

--- src/main/java/com/wdelivery/member/payment/service/PaymentService.java
+++ src/main/java/com/wdelivery/member/payment/service/PaymentService.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 import com.wdelivery.cart.vo.CartVO;
 import com.wdelivery.member.payment.vo.PaymentVO;
+import com.wdelivery.member.payment.vo.ToyCountVO;
 
 public interface PaymentService {
 
 	void insertPaidOrderList(PaymentVO paymentVO, ArrayList<CartVO> cartVO);
+	
+	public void toyCount(ToyCountVO toyCountVO);
 
 }
