1df7c119d7027950a6d0fd9af4fb8adf613e27f0
nes <nes0510@gmail.com>
2021-11-23 06:11:34 +0900

    아드디어 트랜잭션 걸린다

--- src/main/java/com/wdelivery/member/payment/controller/PaymentController.java
+++ src/main/java/com/wdelivery/member/payment/controller/PaymentController.java
@@ -49,7 +49,7 @@ public class PaymentController {
 
 	@Transactional(rollbackFor = Exception.class)
 	@PostMapping("paywinCredit.do")
-	public String paywinDelivery(@RequestBody PaymentVO paymentVO, HttpSession session){
+	public String paywinDelivery(@RequestBody PaymentVO paymentVO, HttpSession session) throws Exception{
 		UserVO userInfo = SessionClassifier.sessionClassifier(session);
 		ArrayList<CartVO> cartVO = TypeSafety.sessionCartCaster(session.getAttribute("cartList"), paymentVO);
 		if (userInfo != null) {
@@ -59,7 +59,9 @@ public class PaymentController {
 		}
 	    paymentVO.setOrder_date(new Date());
 		paymentService.insertPaidOrderList(paymentVO, cartVO);
-
+     
+		//if(userInfo!=null) throw new Exception("아제발 들어와라");
+		
 		for(int i=0; i<cartVO.size(); i++) { //toy
 			if(900 <= cartVO.get(i).getCart_product_code() && cartVO.get(i).getCart_product_code() < 1000) {
 				System.out.println("toy" + cartVO.get(i).getCart_h_code());
