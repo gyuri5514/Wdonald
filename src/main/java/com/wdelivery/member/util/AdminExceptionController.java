659a9d69c09b29f47c683e590e84f81a5dd78ea7
nes <nes0510@gmail.com>
2021-11-26 02:13:18 +0900

    에이

---
+++ src/main/java/com/wdelivery/member/util/AdminExceptionController.java
@@ -0,0 +1,14 @@
+package com.wdelivery.member.util;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice("com.wdelivery.admin")
+public class AdminExceptionController {
+	@ExceptionHandler(Exception.class)
+	public String handler(Exception e,Model model) {
+		model.addAttribute("exception",e);
+		return "404";
+	}
+}
